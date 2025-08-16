9d67477271140b6f3cd3e8d3f35b7dabcc3d5878
Hem B K <baralhems12@gmail.com>
2025-08-16 15:25:24 -0500

    Called Loans and Cards microservice through Feign client

---
+++ service-discovery/accounts/src/main/java/com/sapiustech/accounts/service/client/CardsFeignClient.java
@@ -0,0 +1,17 @@
+package com.sapiustech.accounts.service.client;
+
+import com.sapiustech.accounts.dto.CardsDto;
+import com.sapiustech.accounts.dto.LoansDto;
+import jakarta.validation.constraints.Pattern;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient("cards")
+public interface CardsFeignClient {
+
+    @GetMapping(value = "/api/fetch", consumes = "application/json")
+    public ResponseEntity<CardsDto> fetchCardDetails(@RequestParam
+                                                     String mobileNumber);
+}
---
+++ service-discovery/accounts/src/main/java/com/sapiustech/accounts/service/impl/CustomersServiceImpl.java
@@ -0,0 +1,56 @@
+package com.sapiustech.accounts.service.impl;
+
+import com.sapiustech.accounts.dto.AccountsDto;
+import com.sapiustech.accounts.dto.CardsDto;
+import com.sapiustech.accounts.dto.CustomerDetailsDto;
+import com.sapiustech.accounts.dto.LoansDto;
+import com.sapiustech.accounts.entity.Accounts;
+import com.sapiustech.accounts.entity.Customer;
+import com.sapiustech.accounts.exception.ResourceNotFoundException;
+import com.sapiustech.accounts.mapper.AccountsMapper;
+import com.sapiustech.accounts.mapper.CustomerMapper;
+import com.sapiustech.accounts.repository.AccountsRepository;
+import com.sapiustech.accounts.repository.CustomerRepository;
+import com.sapiustech.accounts.service.ICustomersService;
+import com.sapiustech.accounts.service.client.CardsFeignClient;
+import com.sapiustech.accounts.service.client.LoansFeignClient;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CustomersServiceImpl implements ICustomersService {
+
+
+    private AccountsRepository accountsRepository;
+    private CustomerRepository customerRepository;
+    private CardsFeignClient cardsFeignClient;
+    private LoansFeignClient loansFeignClient;
+
+    /**
+     * @param mobileNumber - Input Mobile Number
+     * @return Customer Details based on a given mobileNumber
+     */
+    @Override
+    public CustomerDetailsDto fetchCustomerDetails(String mobileNumber) {
+        Customer customer = customerRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Customer", "mobileNumber", mobileNumber)
+        );
+        Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId()).orElseThrow(
+                () -> new ResourceNotFoundException("Account", "customerId", customer.getCustomerId().toString())
+        );
+
+        CustomerDetailsDto customerDetailsDto = CustomerMapper.mapToCustomerDetailsDto(customer, new CustomerDetailsDto());
+        customerDetailsDto.setAccountsDto(AccountsMapper.mapToAccountsDto(accounts, new AccountsDto()));
+
+        ResponseEntity<LoansDto> loansDtoResponseEntity = loansFeignClient.fetchLoanDetails(mobileNumber);
+        customerDetailsDto.setLoansDto(loansDtoResponseEntity.getBody());
+
+        ResponseEntity<CardsDto> cardsDtoResponseEntity = cardsFeignClient.fetchCardDetails(mobileNumber);
+        customerDetailsDto.setCardsDto(cardsDtoResponseEntity.getBody());
+
+        return customerDetailsDto;
+
+    }
+}
