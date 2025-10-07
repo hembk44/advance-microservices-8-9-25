5ae976f365e1b97204f81507d6d2546ed670cd7f
Hem B K <baralhems12@gmail.com>
2025-10-07 13:13:20 -0500

    Added Prometheus, opentelemetry and tempo for metrics and traces

--- service-resiliency/cards/src/main/java/com/sapius/cards/controller/CardsController.java
+++ service-resiliency/cards/src/main/java/com/sapius/cards/controller/CardsController.java
@@ -89,8 +89,9 @@ public class CardsController {
             @RequestParam
                                                                @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
                                                                String mobileNumber) {
-        logger.debug("sapius-correlation-id found: {} ", correlationId);
+        logger.debug("fetchCardDetails method start");
         CardsDto cardsDto = iCardsService.fetchCard(mobileNumber);
+        logger.debug("fetchCardDetails method end");
         return ResponseEntity.status(HttpStatus.OK).body(cardsDto);
     }
 
